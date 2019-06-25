302bc373ec4c935bf1a5b344ee167ec5efd6870e
saynowithme <saynowithme@gmail.com>
2019-06-26 04:29:12 +0700

    change database

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -20,16 +20,40 @@ class UsersTableSeeder extends Seeder
                 'name' => "Đặng Trường Trúc",
                 'email' => "truongtrucit@gmail.com",
                 'password' => bcrypt('Abc123!@#'),
+                'user' => "truongtrucit",
+                'technique' => "Web Developer",
+                'phone' => "0969026660",
+                'add' => "47 Nguyễn Tuân",
+                'bio' => "quá đẹp trai",
+                'type' => "1",
+                'status' => "1",
+                
             ],
             [
                 'name' => "Trần Xuân Ba",
                 'email' => "tranxuanbait@gmail.com",
                 'password' => bcrypt('secret'),
+                'user' => "xuanbait",
+                'technique' => "Web Developer",
+                'phone' => "0969026660",
+                'add' => "47 Nguyễn Tuân",
+                'bio' => "quá đẹp trai",
+                'type' => "1",
+                'status' => "1",
+                
             ],
             [
                 'name' => "Nguyễn Thành Long",
                 'email' => "thanhlongit@gmail.com",
                 'password' => bcrypt('secret'),
+                'user' => "thanhlongit",
+                'technique' => "Web Developer",
+                'phone' => "0969026660",
+                'add' => "47 Nguyễn Tuân",
+                'bio' => "quá đẹp trai",
+                'type' => "1",
+                'status' => "1",
+                
             ],
         ]);
     }
